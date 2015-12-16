cb8e9a7bdfe2ba26c280384c617e7df01b82ec96
Mike Mohler <mike@recordedfuture.com>
2015-12-16 09:34:57 -0500

    Updated comments.

--- metadata-examples/Print_RF_Metadata.py
+++ metadata-examples/Print_RF_Metadata.py
@@ -1,4 +1,5 @@
-"""  This is an example Python script that demonstrates how to query for RF Metadata via the RF API """
+"""  This is an example Python script that demonstrates how to query for RF Metadata via the RF API
+"""
 from RFAPI import RFAPI
 
 """ Token and Metadata Query """
@@ -7,15 +8,15 @@ q ={"metadata":{}}
 
 def print_primitive_or_declared_type(attrs):
     if attrs['type']['kind'] == "primitive" or attrs['type']['kind'] == "declared":
-        """ Primitive or Declared type attribute.  Print the attribute name and the name of the attribute type """
+        # Primitive or Declared type attribute.  Print the attribute name and the name of the attribute type 
         print "  "+attrs['name']+": "+attrs['type']['name']
 
 def print_object_type(attrs):
     if attrs['type']['kind'] == "object":
-        """ Object attribute.  Print attribute name and its type (object) """
+        # Object attribute.  Print attribute name and its type (object) 
         print "  "+attrs['name']+": object"
 
-def getTypesFromList(types_list):
+def get_types_from_list(types_list):
     types = []
     for type in types_list: 
         types.append(type['name'])
@@ -23,22 +24,22 @@ def getTypesFromList(types_list):
     return type_list 
 
 def print_set_type(attrs):
-    """ Sets can contain a Union.  Need to check for that. """
+    # Sets can contain a Union.  Need to check for that. 
     if attrs['type']['kind'] == "set": 
-        """ Set. Print attribute name, and the name of the type name within the Set attribute itself  """
+        # Set. Print attribute name, and the name of the type name within the Set attribute itself 
         if 'name' in attrs['type']['type']:
             print "  "+attrs['name']+": set("+attrs['type']['type']['name']+")"
         elif attrs['type']['type']['kind'] == "union": 
-            """ This is set of a union """
-            print "  "+attrs['name']+": set(union("+getTypesFromList(attrs['type']['type']['types'])+"))"
+            # This is set of a union 
+            print "  "+attrs['name']+": set(union("+get_types_from_list(attrs['type']['type']['types'])+"))"
         else:
             print "  "+attrs['name']+": Unknown?"
 
 def print_union_types(attrs):
     union_type =""
     if attrs['type']['kind'] == "union": 
-        """ Union.  Contains list of types.  Loop over all types """
-        union_string = "  "+attrs['name']+" union("+getTypesFromList(attrs['type']['types'])+")"
+        # Union.  Contains list of types.  Loop over all types 
+        union_string = "  "+attrs['name']+" union("+get_types_from_list(attrs['type']['types'])+")"
         print union_string
 
 """ Print out metadata names/type attributes for a parent metadata element """
@@ -50,23 +51,23 @@ def print_attributes(attrs):
 
 """ Main Routine """
 def main():
-    """ Construct a RFAPI query object """
+    # Construct a RFAPI query object 
     rfqapi = RFAPI(TOKEN)
 
-    """ Query for the metadata """
+    # Query for the metadata 
     mdata_result = rfqapi.paged_query(q)
 
-    """ Loop over all the metadata and each metadata attributes """
+    # Loop over all the metadata and each metadata attributes 
     for metadata in mdata_result:
         mdata_types =  metadata['types']
         for md_type in mdata_types:
-            """ Print each Root Metadata Type """
+            # Print each Root Metadata Type 
             parent_type=""
             if 'parent' in md_type:
                 parent_type = str(md_type['parent'])
             print md_type['name']+"("+parent_type+")"
 
-            """ Loop over attributes in this metadata type and print their corresponding types """
+            # Loop over attributes in this metadata type and print their corresponding types 
             for md_attr_list in md_type['attrs']:
                 print_attributes(md_attr_list)
 
